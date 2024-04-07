a61ca3ad8028b8b1423cab81cb64c2172503d830
prashant kumar singh <prashant.singh2012p@gmail.com>
2024-04-07 17:08:54 +0530

    code chnaged

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Projects"
+    return "CICD are stablished"
 
 
 if __name__== "__main__":
